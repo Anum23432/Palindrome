4f36e788322e444a4df91a7889f0a7494f0fb535
Anum23432 <83118167+Anum23432@users.noreply.github.com>
2021-06-28 20:22:08 +0500

    Palindrome

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Palindrome
+FlipWordChecker (Palindrome App)
+​
+A repo having an app of Flip Word Checker.
+It will check whether the input word is palindrome or not.​
+
+- Technologies used
+HTML5
+CSS3
+JavaScript (for coding algorithms)
+
+- Software needed to run this app
+VSCode (as an editor)
+Google Chrome (UI)
+
+- How does the App work:
+Input any English word.
+Click on the "Check" button.
+Find the result below.
---
+++ app.js
@@ -0,0 +1,21 @@
+document.getElementById("btn").addEventListener("click", function(){
+    let text = document.getElementById("input").value;
+    console.log(text);
+    checkPalindrome(text);
+});
+function checkPalindrome(text){
+let word = text.replace(/[^a-zA-Z0-9]/g,  '').toLowerCase();
+console.log(text,word);
+let len = word.length;
+let half = Math.floor(len/2);
+let result = document.getElementById("result");
+let i;
+
+for (i=0; i<half; i++){
+    if(word[i] !== word[len-1-i]){
+        result.textContent = "NO! This word is not palindrome.";
+        return;
+    }
+    result.textContent = "OH YES! Given word is palindrome."
+}
+}

beae1c7838c295ef8dc204a3a3a789a3fc9d4a6a
Anum23432 <83118167+Anum23432@users.noreply.github.com>
2021-08-11 14:54:05 +0500

    Palindrome

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ console.log(text,word);
 let len = word.length;
 let half = Math.floor(len/2);
 let result = document.getElementById("result");
-let i;
 
-for (i=0; i<half; i++){
+
+for (let i=0; i<half; i++){
     if(word[i] !== word[len-1-i]){
         result.textContent = "NO! This word is not palindrome.";
         return;
